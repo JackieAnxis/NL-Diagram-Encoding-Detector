5850a1261089d3b4f5cb98651a73486b91dbd980
YingchaojieFeng <fengyingchaojie@163.com>
2021-02-25 19:14:19 +0800

    feat: 🎸add data encoder and decoder

--- src/index.js
+++ src/index.js
@@ -56,14 +56,56 @@ function detector(code, data) {
  * @param {JSON format data} originData
  * @return {JSON format data} encodedData
  */
-function dataEncoder(originData) {}
+function dataEncoder(originData) {
+    let encodedData = {...originData}
+    const keys = ['nodes', 'links']
+    keys.forEach(key => {
+        encodedData[key] = encodedData[key].map(ele => {
+            let newEle = {}
+            Object.keys(ele).forEach(attrName => {
+                if (ele[attrName] instanceof Object){
+                    Object.keys(ele[attrName]).forEach(attrSubName => {
+                        newName = attrName + '-' + attrSubName
+                        newEle[newName] = ele[attrName][attrSubName]
+                    })
+                }else{
+                    newEle[attrName] = ele[attrName]
+                }
+            })
+            return newEle
+        })
+    })
+    return encodedData
+}
 
 /**
  * The opposite of dataEncoder
  * @param {JSON format data} encodedData
  * @return {JSON formated data}
  */
-function dataDecoder(encodedData) {}
+function dataDecoder(encodedData) {
+    let originData = {...encodedData}
+    const keys = ['nodes', 'links']
+    keys.forEach(key => {
+        originData[key] = originData[key].map(ele => {
+            let newEle = {}
+            Object.keys(ele).forEach(attrName => {
+                if (attrName.indexOf('-') != -1){
+                    let [supName, subName] = attrName.split('-')
+                    subName = isNaN(Number(subName))? subName: Number(subName)
+                    if (Object.keys(newEle).indexOf(supName) == -1){
+                        newEle[supName] = isNaN(Number(subName))? {}: []
+                    }
+                    newEle[supName][subName] = ele[attrName]
+                }else{
+                    newEle[attrName] = ele[attrName]
+                }
+            })
+            return newEle
+        })
+    })
+    return originData
+}
 
 /**
  * get nodes/links related visual elements and their counts
